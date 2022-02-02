2ed3d6352e3b532a2fec4d90eb9ae4549b9e2182
shoreofwonder <shoreofwonder@gmail.com>
2022-02-02 17:15:26 -0500

    ai and ui are much better now

--- wordly/main.py
+++ wordly/main.py
@@ -1,8 +1,53 @@
 import random
+from collections import Counter
 
 from wordly.game import make_guess
 from wordly.solver import Solver
+from wordly.util import ColoredText
 from wordly.word_list import all_wordle_words, common_wordle_words_4k
+
+
+def render_game(guesses: list):
+    print("")
+
+    for guess, result in guesses:
+        s = []
+        for i, c in enumerate(guess):
+            if result[i] == '?':
+                # inexact match
+                s.append(ColoredText.WARNING + c + ColoredText.ENDC)
+            elif result[i] == '.':
+                # nonmatch
+                s.append(ColoredText.FAIL + c + ColoredText.ENDC)
+            else:
+                # match
+                s.append(ColoredText.OKGREEN + c + ColoredText.ENDC)
+        print(' '*12 + ' '.join(s))
+    n_blank = 6 - len(guesses)
+    for _ in range(n_blank):
+        print(' '*12 + ' '.join(['_']*5))
+    print('')
+
+
+def keyboard_str(guesses: list) -> str:
+    letters = 'abcdefghijklmnopqrstuvwxyz'.upper()
+    letter_colors = dict(zip(letters, ['']*26))
+    for guess, result in guesses:
+        for i, c in enumerate(guess):
+            if result[i] == '?':
+                # inexact match
+                letter_colors[c] = ColoredText.WARNING
+            elif result[i] == '.':
+                # nonmatch
+                letter_colors[c] = ColoredText.FAIL
+            else:
+                # match
+                letter_colors[c] = ColoredText.OKGREEN
+    ret = []
+    for l in letters:
+        ret.append(letter_colors[l] + l + ColoredText.ENDC)
+    return ' '*3 + '(' + ''.join(ret) + ')'
+
 
 def play_interactive():
     """
@@ -10,28 +55,39 @@ def play_interactive():
     Targets will be in the 4000 most common English words that are in the
     Wordle dictionary, because solutions like ESNES and ROLAG are rage-inducing.
     """
+    print("\n-----=====  It's Wordly Time!  =====-----")
+    print("Guess a five-letter word, or ? for a hint.")
     valid = set(x[0] for x in all_wordle_words)
     target = random.choice(common_wordle_words_4k)
+    guesses = []
     while True:
-        w = input('> ')
+        render_game(guesses)
+        w = input(keyboard_str(guesses) + ' > ')
         w = w.upper()
+
         if w == "?":  # hax
-            print(target)
+            s = Solver()
+            ai_words = s.get_next_words(dict(guesses))
+            print(ai_words)
+            ai_word = ai_words[0][0]
+            print('   Try the word "{}".'.format(ai_word))
             continue
 
         if len(w) != 5 or w not in valid:
             print('Invalid word:', w)
             continue
 
+        result = make_guess(w, target)
+        guesses.append((w, result))
+
         if w == target:
+            render_game(guesses)
             print('you win!')
             return
-        else:
-            print(' ', make_guess(w, target))
 
-def solve():
+
+def solve(s: Solver):
     target, _ = random.choice(all_wordle_words)
-    s = Solver(hard_mode=False)
     guesses = {}
     guess = random.choice(common_wordle_words_4k)
     while True:
@@ -41,9 +97,8 @@ def solve():
         print(result)
         if guess == target:
             print('got it in', len(guesses), 'guesses')
-            return
+            return len(guesses)
         # update pools based on guesses
-        s.valids.apply_nonmatches(guess, result)
         s.targets.apply_guesses(guesses)
         if len(s.targets.pool) == 1:
             guess = s.targets.pool.pop()
@@ -53,5 +108,50 @@ def solve():
             guess = next_words[0][0]
 
 
-play_interactive()
-# solve()
+def fit_params():
+    all_results = []
+    N_TRIALS = 1000
+    cost_exps = [1.75]
+    mpoolsizes = [5000]
+    gt_ratios = [1.0]
+    modes = [True, False]
+    import math
+    tot = math.prod([len(x) for x in [cost_exps, mpoolsizes, gt_ratios, modes]])
+    tot *= N_TRIALS
+    print('running', tot, 'trials')
+    tnum = 0
+    for hard_mode in modes:
+        for cost_exp in cost_exps:
+            for max_pool_size in mpoolsizes:
+                for gt_ratio in gt_ratios:
+
+                    results = []
+                    for i in range(N_TRIALS):
+                        tnum += 1
+                        if tnum % 10 == 2:
+                            print(round(tnum / tot * 100, 3), '% done')
+                        s = Solver(hard_mode=hard_mode,
+                                   max_pool_size=max_pool_size,
+                                   gt_ratio=gt_ratio,
+                                   cost_exp=cost_exp)
+                        result = solve(s)
+                        results.append(result)
+                    counts = Counter(results)
+                    losses = 0
+                    mean = round(sum(results) / len(results), 3)
+                    for k in counts.keys():
+                        if k > 6:
+                            losses += 1
+                    losses = round(losses / len(results) * 100, 3)
+                    all_results.append((hard_mode, cost_exp, max_pool_size, gt_ratio, mean, losses))
+    print('hard_mode, cost_exp, max_pool_size, gt_ratio, mean, losses')
+    for r in all_results:
+        print(str(r) + ',')
+    print(tot)
+
+
+if __name__ == '__main__':
+    # play_interactive()
+    fit_params()
+    #s = Solver(hard_mode=False)
+    #solve(s)
--- wordly/solver.py
+++ wordly/solver.py
@@ -1,10 +1,11 @@
 import copy
 import random
+import time
+from typing import *
 
 from wordly.game import make_guess
-from wordly.util import ALL_POSSIBLE_RESULTS
 from wordly.word_pool import WordPool
-from wordly.word_list import all_wordle_words
+from wordly.word_list import all_wordle_words, top_first_guesses
 
 """
 The Solver suggests a next word based on all available information.
@@ -24,7 +25,7 @@ As a human player, these strategies intuitive.
 [2] http://sonorouschocolate.com/notes/index.php?title=The_best_strategies_for_Wordle
 """
 class Solver():
-    def __init__(self, hard_mode=False):
+    def __init__(self, hard_mode=False, cost_exp=2, max_pool_size=5000, gt_ratio=1):
         self.hard_mode = hard_mode
         # targets represents the remaining possible solutions to a puzzle after applying guesses
         self.targets = WordPool()
@@ -35,17 +36,80 @@ class Solver():
         for word, freq in all_wordle_words:
             self.freqs[word] = freq
 
-    def get_next_words(self, guesses):
-        # Try each combination of (valid word, target)
-        # Find the one that most likely constrains the target pool to very few / very common words
+        # params that control the heuristics
+        # these have been tuned to produce the best results
+        # while still solving the puzzle quickly
+        self.cost_exp = cost_exp
+        self.max_pool_size = max_pool_size
+        self.gt_ratio = gt_ratio
+
+    def get_next_words(self, guesses) -> List[Tuple[str, int]]:
+        # Returns a list of good words to guess next
+
+        # First guesses are pre-baked; otherwise that's a really long search
+        # that's the same every game.
+        if len(guesses) == 0:
+            random.shuffle(top_first_guesses)
+            return [(x, 1) for x in top_first_guesses]
+
+        # apply guess feedback to eliminate invalid targets and valid guesses.
         self.targets.apply_guesses(guesses)
         if self.hard_mode:
             self.valids.apply_hardmode_constraints(guesses)
 
-        guess_scores = []
-        best_score = float('inf')
-        guess_pool = random.sample(list(self.valids.pool), min(len(self.valids.pool), 25))
-        targets_pool = list(self.targets.pool)
+        # Handle edge case: if there are only two targets left, you don't want to pick
+        # "any guess that reduces the target pool to 1", you want to pick
+        # one of the two possible solutions, thereby profiting from the 50/50 chance
+        # of guessing correctly.
+        if len(self.targets.pool) <= 2:
+            return [(random.choice(list(self.targets.pool)), 1)]
+
+        # For non-first guesses, we'll do a search.
+        # Ideally, we will try each combination of (valid word, target) and
+        # find the one that most likely constrains the target pool to very few words.
+        # If the guess and target pools are large, we do some heuristic
+        # stuff to reduce or subsample the space before running the all-pairs search.
+        targets_subset = copy.deepcopy(self.targets)
+        valids_subset = copy.deepcopy(self.valids)
+        pool_size = len(self.valids.pool) * len(self.targets.pool)
+
+        # Each pair takes around 0.2ms to check, so we want to reduce pool_size to <= 5000.
+        # Start by applying nonmatches from the guess results to the guesses pool. That will
+        # encourage guessing of new letters.
+        if pool_size > self.max_pool_size:
+            # reduce valid guesses pool by removing words with nonmatching letters
+            ls = list((k, v) for k, v in guesses.items())
+            random.shuffle(ls)
+            for guess, result in ls:
+                if len(valids_subset.pool) * len(targets_subset.pool) <= self.max_pool_size:
+                    # good enough
+                    break
+                before = copy.deepcopy(valids_subset)
+                valids_subset.apply_nonmatches(guess, result)
+                if len(valids_subset.pool) < 50:
+                    # Over-constraining the guess pool leads to dumb guesses
+                    # don't go too hard on this
+                    valids_subset = before
+
+        # Subsample the guess and target pools as needed to shrink from there.
+        while pool_size > self.max_pool_size:
+            if len(valids_subset.pool) >= len(targets_subset.pool) * self.gt_ratio:
+                # reduce the guesses pool
+                vpool = random.sample(list(valids_subset.pool),
+                                      max(len(valids_subset.pool) // 2, 10)
+                                      )
+                valids_subset.remove_all_except(vpool)
+            else:
+                # reduce the targets pool
+                tpool = random.sample(list(targets_subset.pool),
+                                      max(len(targets_subset.pool)//2, 10)
+                                      )
+                targets_subset.remove_all_except(tpool)
+            pool_size = len(targets_subset.pool) * len(valids_subset.pool)
+
+        t0 = time.time()
+
+
         """
         if len(self.targets.pool) > 243:  # 3^5
             # If there are many targets, we can simply try all guess results.
@@ -68,21 +132,34 @@ class Solver():
                     guess_scores.append((guess, guess_score))
         else:
         """
+
+        # now search all (valid guess, target) pairs to find the best guess from the
+        # reduced pools.
+        guess_scores = []
+        best_score = float('inf')
+        guess_pool = list(valids_subset.pool)
+        targets_pool = list(targets_subset.pool)
+
         for guess in guess_pool:
             guess_score = 0
             guesses_copy = copy.deepcopy(guesses)
-            random.shuffle(targets_pool)
-            for tw in targets_pool[:25]:
-                targets_copy = copy.deepcopy(self.targets)
+            for i, tw in enumerate(targets_pool):
+                targets_copy = copy.deepcopy(targets_subset)
                 new_guesses = {guess: make_guess(guess, tw)}
                 new_guesses.update(guesses)
-                targets_copy.apply_guesses(guesses_copy)
-                guess_score += len(targets_copy.pool)
+                n_before = len(targets_copy.pool)
+                targets_copy.apply_guesses(new_guesses)
+                n_left = len(targets_copy.pool)
+                guess_score += n_left**self.cost_exp
                 if guess_score > best_score:
+                    # we already know this guess is worse than one we have
+                    # bail outta this trial
                     break
             if guess_score < best_score:
-                guess_scores.append((guess, guess_score))
+                best_score = guess_score
+                guess_scores.append((guess, round(guess_score, 3)))
 
+        #print(len(valids_subset.pool), len(targets_subset.pool), pool_size, round(time.time()-t0, 3))
         guess_scores.sort(key=lambda x: x[1])
         return guess_scores
 
